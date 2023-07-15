29b4fe582fa5a03a1434867ec247456adf973b89
Pedro Henrique <pedrohrds05@hotmail.com>
2023-07-15 17:54:48 -0300

    feat: adding functions to query db

--- src/main/java/br/com/banco/repositories/TransferenciaRepository.java
+++ src/main/java/br/com/banco/repositories/TransferenciaRepository.java
@@ -1,9 +1,26 @@
 package br.com.banco.repositories;
 
+import java.time.OffsetDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.banco.entites.Transferencia;
 
 public interface TransferenciaRepository extends JpaRepository<Transferencia, Long> {
+    @Query("SELECT t FROM Transferencia t WHERE t.conta.id = :accountId")
+    List<Transferencia> findAllByAccountId(Long accountId);
 
+    @Query("SELECT t FROM Transferencia t WHERE t.transactionOperatorName = :operatorName AND t.conta.id = :accountId")
+    List<Transferencia> findAllByOperatorNameAndContaId(String operatorName, Long accountId);
+    
+    @Query("SELECT t FROM Transferencia t WHERE t.transferDate BETWEEN :initialDate AND :finalDate AND t.conta.id = :accountId")
+    List<Transferencia> findAllByAccountIdBetweenTransferDate(OffsetDateTime initialDate, OffsetDateTime finalDate, Long accountId);
+    
+    @Query("SELECT t FROM Transferencia t WHERE t.transactionOperatorName = :operatorName " +
+            "AND t.transferDate BETWEEN :initialDate AND :finalDate " +
+            "AND t.conta.id = :accountId"
+     )
+    List<Transferencia> findAllByOperatorNameAndAccountIdBetweenTransferDate(String operatorName, OffsetDateTime initialDate, OffsetDateTime finalDate, Long accountId);
 }
