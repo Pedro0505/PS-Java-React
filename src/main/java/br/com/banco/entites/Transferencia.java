2069a09e0f26369d901c3844c491e784a26c2378
Pedro Henrique <pedrohrds05@hotmail.com>
2023-07-15 17:56:10 -0300

    refactor: adding getter and setter to id in Transferencia entitie

--- src/main/java/br/com/banco/entites/Transferencia.java
+++ src/main/java/br/com/banco/entites/Transferencia.java
@@ -56,6 +56,15 @@ public class Transferencia {
 	public void setTransferDate(OffsetDateTime transferDate) {
 		this.transferDate = transferDate;
 	}
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public Double getValue() {
 		return value;
