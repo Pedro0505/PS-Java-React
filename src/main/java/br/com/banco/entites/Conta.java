6143441f2d6842fd51285c4c995a7380b78d640f
Pedro Henrique <pedrohrds05@hotmail.com>
2023-07-15 10:40:24 -0300

    refactor: refactoring entites and adding getters and setters

--- src/main/java/br/com/banco/entites/Conta.java
+++ src/main/java/br/com/banco/entites/Conta.java
@@ -15,17 +15,8 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 @Entity()
 @Table(name = "conta")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Conta {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -38,4 +29,38 @@ public class Conta {
 	@JsonIgnore
 	@OneToMany(mappedBy = "conta",fetch = FetchType.LAZY, cascade = CascadeType.REMOVE, orphanRemoval = true)
 	private List<Transferencia> transferencia = new ArrayList<>();
+
+	public Conta() {
+	}
+
+	public Conta(Long id, String name, List<Transferencia> transferencia) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.transferencia = transferencia;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Transferencia> getTransferencia() {
+		return transferencia;
+	}
+
+	public void setTransferencia(List<Transferencia> transferencia) {
+		this.transferencia = transferencia;
+	}
 }
--- src/main/java/br/com/banco/entites/Transferencia.java
+++ src/main/java/br/com/banco/entites/Transferencia.java
@@ -11,17 +11,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 @Entity()
 @Table(name = "transferencia")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 public class Transferencia {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,8 +30,62 @@ public class Transferencia {
 
 	@Column(name = "nome_operador_transacao", nullable = false, length = 50)
 	private String transactionOperatorName;
-	
+
 	@ManyToOne()
 	@JoinColumn(name = "conta_id")
 	private Conta conta;
+
+	public Transferencia() {
+	}
+
+	public Transferencia(Long id, OffsetDateTime transferDate, Double value, String type,
+			String transactionOperatorName, Conta conta) {
+		super();
+		this.id = id;
+		this.transferDate = transferDate;
+		this.value = value;
+		this.type = type;
+		this.transactionOperatorName = transactionOperatorName;
+		this.conta = conta;
+	}
+
+	public OffsetDateTime getTransferDate() {
+		return transferDate;
+	}
+
+	public void setTransferDate(OffsetDateTime transferDate) {
+		this.transferDate = transferDate;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getTransactionOperatorName() {
+		return transactionOperatorName;
+	}
+
+	public void setTransactionOperatorName(String transactionOperatorName) {
+		this.transactionOperatorName = transactionOperatorName;
+	}
+
+	public Conta getConta() {
+		return conta;
+	}
+
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
 }
