75ab6cda19b7a5933c35f030d6218ac75f44bd68
Pedro Henrique <pedrohrds05@hotmail.com>
2023-07-17 01:09:08 -0300

    feat: adding valiations to date
    validations to test if initialDate is after finaldate
    validations to test if the dates is send

--- src/main/java/br/com/banco/services/TransferenciaService.java
+++ src/main/java/br/com/banco/services/TransferenciaService.java
@@ -7,8 +7,10 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.banco.dto.TransferenciaSearchDto;
 import br.com.banco.entites.Transferencia;
 import br.com.banco.repositories.TransferenciaRepository;
+import br.com.banco.services.exception.BadRequestException;
 import br.com.banco.utils.SerializeDates;
 
 @Service
@@ -16,44 +18,72 @@ public class TransferenciaService {
 	@Autowired
 	private TransferenciaRepository repository;
 
-	public List<Transferencia> getAllFiltered(Optional<String> operatorName, Optional<String> initialDate, Optional<String> finalDate, String accountId) {
-		String startDateTime = initialDate.orElse(null);
-		String endDateTime = finalDate.orElse(null);
-		String operator = operatorName.orElse(null);
-		Long id = Long.parseLong(accountId);
-
+	public List<Transferencia> getAllFiltered(Optional<String> operatorName, Optional<String> initialDate, Optional<String> finalDate, Optional<String> accountId) {
+		TransferenciaSearchDto transferencia = this.transferenciaSerialize(operatorName, initialDate, finalDate, accountId);
+		
+		OffsetDateTime startDateTime = transferencia.getInitialDate();
+		OffsetDateTime endDateTime = transferencia.getFinalDate();
+		String operator = transferencia.getOperatorName();
+		
+		this.verifyDates(startDateTime, endDateTime);
+		
 		if (startDateTime != null && endDateTime != null && operator != null) {
-			return this.getAllByDateRangeAndOperatorName(operator, startDateTime, endDateTime, id);
-		} else if (startDateTime != null && endDateTime != null) {
-			return this.getAllByDateRange(startDateTime, endDateTime, id);
+			return this.getAllByDateRangeAndOperatorName(transferencia);
+		} else if (startDateTime != null && endDateTime != null) {			
+			return this.getAllByDateRange(transferencia);
 		} else if (operator != null) {
-			return this.getAllByOperatorName(operator, id);
+			return this.getAllByOperatorName(transferencia);
 		} else {
-			return this.getAllByAccountId(id);
+			return this.getAllByAccountId(transferencia);
 		}
 	}
 
-	public List<Transferencia> getAllByAccountId(Long accountId) {
-		return this.repository.findAllByAccountId(accountId);
+	public List<Transferencia> getAllByAccountId(TransferenciaSearchDto transferencia) {
+		return this.repository.findAllByAccountId(transferencia.getAccountId());
 	}
 
-	public List<Transferencia> getAllByOperatorName(String operatorName, Long accountId) {
-		return this.repository.findAllByOperatorNameAndContaId(operatorName, accountId);
+	public List<Transferencia> getAllByOperatorName(TransferenciaSearchDto transferencia) {
+		return this.repository.findAllByOperatorNameAndContaId(transferencia.getOperatorName(), transferencia.getAccountId());
 	}
 
-	public List<Transferencia> getAllByDateRange(String initialDate, String finalDate, Long accountId) {
-		OffsetDateTime startDate = SerializeDates.stringToOffSetDateTime(initialDate);
-		OffsetDateTime endDate = SerializeDates.stringToOffSetDateTime(finalDate);
+	public List<Transferencia> getAllByDateRange(TransferenciaSearchDto transferencia) {
+		OffsetDateTime startDateTime = transferencia.getInitialDate();
+		OffsetDateTime endDateTime = transferencia.getFinalDate();
 
-		return this.repository.findAllByAccountIdBetweenTransferDate(startDate, endDate, accountId);
+		this.finalDateIsAfter(startDateTime, endDateTime);
+
+		return this.repository.findAllByAccountIdBetweenTransferDate(startDateTime, endDateTime, transferencia.getAccountId());
 	}
 
-	public List<Transferencia> getAllByDateRangeAndOperatorName(String operatorName, String initialDate,
-			String finalDate, Long accountId) {
-		OffsetDateTime startDate = SerializeDates.stringToOffSetDateTime(initialDate);
-		OffsetDateTime endDate = SerializeDates.stringToOffSetDateTime(finalDate);
+	public List<Transferencia> getAllByDateRangeAndOperatorName(TransferenciaSearchDto transferencia) {
+		OffsetDateTime startDateTime = transferencia.getInitialDate();
+		OffsetDateTime endDateTime = transferencia.getFinalDate();
+		String operator = transferencia.getOperatorName();
 
-		return this.repository.findAllByOperatorNameAndAccountIdBetweenTransferDate(operatorName, startDate, endDate,
-				accountId);
+		this.finalDateIsAfter(startDateTime, endDateTime);
+
+		return this.repository.findAllByOperatorNameAndAccountIdBetweenTransferDate(operator, startDateTime, endDateTime,
+				transferencia.getAccountId());
+	}
+	
+	private void finalDateIsAfter(OffsetDateTime initialDate, OffsetDateTime finalDate) {
+		if (initialDate.isAfter(finalDate)) {
+			throw new BadRequestException("A data de fim não pode ser anterior à data de início.");
+		}
+	}
+	
+	private void verifyDates(OffsetDateTime initialDate, OffsetDateTime finalDate) {
+		if ((initialDate != null && finalDate == null) || (initialDate == null && finalDate != null)) {
+			throw new BadRequestException("Se uma data for informada, a outra também precisa ser preenchida.");
+		}
+	}
+	
+	private TransferenciaSearchDto transferenciaSerialize(Optional<String> operatorName, Optional<String> initialDate, Optional<String> finalDate, Optional<String> accountId) {
+		OffsetDateTime startDateTime = initialDate.map(SerializeDates::stringToOffSetDateTime).orElse(null);
+		OffsetDateTime endDateTime = finalDate.map(SerializeDates::stringToOffSetDateTime).orElse(null);
+		String operator = operatorName.orElse(null);
+		Long id = accountId.map(Long::parseLong).orElse(null);
+
+		return new TransferenciaSearchDto(id, startDateTime, endDateTime, operator);
 	}
 }
