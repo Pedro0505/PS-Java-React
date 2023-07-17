5c00f8e253d1f0c3f04d402b8cce8b8e0320280c
Pedro Henrique <pedrohrds05@hotmail.com>
2023-07-17 01:07:27 -0300

    test: testing error in validations

--- src/test/java/br/com/banco/e2e/TransferenciaTest.java
+++ src/test/java/br/com/banco/e2e/TransferenciaTest.java
@@ -89,4 +89,58 @@ class TransferenciaTest {
 	        .andExpect(jsonPath("$[0].conta.id").value(1))
 	        .andExpect(jsonPath("$[0].transferDate").value(Matchers.startsWith(("2020"))));
 	}
+	
+	@Test
+	@Order(5)
+	public void testGetAllTransactionByAccoutIdOperatorNameAndDateRange_whenPassFinalDateAfterInitial() throws Exception {
+	    this.mockMvc.perform(get(this.baseUrl.concat("&initialDate=2023-07-15&finalDate=2018-01-01&operatorName=Beltrano")).contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().is(HttpStatus.BAD_REQUEST.value()))
+        .andExpect(jsonPath("$.message").value("A data de fim não pode ser anterior à data de início."))
+        .andExpect(jsonPath("$.error").value("Error in your request"));
+	}
+	
+	@Test
+	@Order(6)
+	public void testGetAllTransactionByAccoutIdAndDateRange_whenPassFinalDateAfterInitial() throws Exception {
+	    this.mockMvc.perform(get(this.baseUrl.concat("&initialDate=2023-07-15&finalDate=2018-01-01")).contentType(MediaType.APPLICATION_JSON))
+	        .andExpect(status().is(HttpStatus.BAD_REQUEST.value()))
+	        .andExpect(jsonPath("$.message").value("A data de fim não pode ser anterior à data de início."))
+	        .andExpect(jsonPath("$.error").value("Error in your request"));
+	}
+	
+	@Test
+	@Order(7)
+	public void testGetAllTransactionByAccoutIdAndDateRange_whenDontSendInitialDate() throws Exception {
+	    this.mockMvc.perform(get(this.baseUrl.concat("&finalDate=2018-01-01&operatorName=Beltrano")).contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().is(HttpStatus.BAD_REQUEST.value()))
+        .andExpect(jsonPath("$.message").value("Se uma data for informada, a outra também precisa ser preenchida."))
+        .andExpect(jsonPath("$.error").value("Error in your request"));
+	}
+	
+	@Test
+	@Order(8)
+	public void testGetAllTransactionByAccoutIdAndDateRange_whenDontSendFinalDate() throws Exception {
+	    this.mockMvc.perform(get(this.baseUrl.concat("&initialDate=2023-07-15")).contentType(MediaType.APPLICATION_JSON))
+	        .andExpect(status().is(HttpStatus.BAD_REQUEST.value()))
+	        .andExpect(jsonPath("$.message").value("Se uma data for informada, a outra também precisa ser preenchida."))
+	        .andExpect(jsonPath("$.error").value("Error in your request"));
+	}
+	
+	@Test
+	@Order(9)
+	public void testGetAllTransactionByAccoutIdOperatorNameAndDateRange_whenDontSendInitialDate() throws Exception {
+	    this.mockMvc.perform(get(this.baseUrl.concat("&finalDate=2018-01-01&operatorName=Beltrano")).contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().is(HttpStatus.BAD_REQUEST.value()))
+        .andExpect(jsonPath("$.message").value("Se uma data for informada, a outra também precisa ser preenchida."))
+        .andExpect(jsonPath("$.error").value("Error in your request"));
+	}
+	
+	@Test
+	@Order(10)
+	public void testGetAllTransactionByAccoutIdOperatorNameAndDateRange_whenDontSendFinalDate() throws Exception {
+	    this.mockMvc.perform(get(this.baseUrl.concat("&initialDate=2023-07-15&operatorName=Beltrano")).contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().is(HttpStatus.BAD_REQUEST.value()))
+        .andExpect(jsonPath("$.message").value("Se uma data for informada, a outra também precisa ser preenchida."))
+        .andExpect(jsonPath("$.error").value("Error in your request"));
+	}
 }
